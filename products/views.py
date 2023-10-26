a44e2a4ca2cd7a362a93e8ec65599c814ffdc005
Vladimir <vov4ic-123@mail.ru>
2023-10-26 21:49:47 +0400

    Работать с ORM, моделями, БД
    Были созданы модели Product, ProductCategory в products/models.py; Проведена миграция; Был создан суперюзер; Регистрация моделей в admin; Изменен контролер products в products/view.py для использование выше созданных моделей; Создана папка media, в mystore/settings.py добавлены MEDIA_URL, MEDIA_ROOT; в mystore/urls.py добавлено условие для media при DEBUG

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 
 
 def index(request):
@@ -7,33 +8,7 @@ def index(request):
 
 def products(request):
     context = {'title': 'Store',
-               'products': [
-                   {
-                       'name': 'Худи черного цвета с монограммами adidas Originals',
-                       'imagine': 'static/vendor/img/products/Adidas-hoodie.png',
-                       'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-                       'price': 6090,
-                   },
-
-                   {
-                       'name': 'Синяя куртка The North Face',
-                       'imagine': 'static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                       'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                       'price': 23725,
-                   },
-
-                   {
-                       'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                       'imagine': 'static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                       'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                       'price': 3390,
-                   },
-
-                   {
-                       'name': 'Синяя куртка The North Face',
-                       'imagine': 'static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                       'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                       'price': 23725,
-                   }
-               ]}
-    return render(request, 'products/products.html')
+               'products': Product.objects.all(),
+               'categories': ProductCategory.objects.all(),
+               }
+    return render(request, 'products/products.html', context=context)
